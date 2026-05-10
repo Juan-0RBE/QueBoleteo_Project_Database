96fed4f0dfb8472a6213191c5962ba522249d6f1
Juan-0RBE <juanorbe2006@gmail.com>
2026-05-10 11:06:33 -0500

    SpringBoot Back
    A basic Spring Boot setup for backend development

---
+++ queboleteo/src/main/java/co/edu/unbosque/queboleteo/QueboleteoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.queboleteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueboleteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueboleteoApplication.class, args);
+	}
+
+}
